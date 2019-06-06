57c8503275d0fc73b00ef0f0fab7a68d09f909af
ArCiGo <armandocifuentes_1@hotmail.com>
2019-06-06 16:50:50 -0500

    Add of test for ToDO app

---
+++ cypress/integration/form-input.spec.js
@@ -0,0 +1,17 @@
+describe("Form input", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("Visits the app", () => {
+    cy.focused().should("have.class", "new-todo");
+  });
+
+  it.only("Accepts input", () => {
+    const typedText = "New todo";
+
+    cy.get(".new-todo")
+      .type(typedText)
+      .should("have.value", typedText);
+  });
+});
